7925975d0917153571b7e20da9b9f9b6e3361ce5
mito <orixbaffaloes@gmail.com>
2025-09-17 11:02:16 +0900

    CD検証のためEC2を追加

--- aws-study-tf/modules/ec2/main.tf
+++ aws-study-tf/modules/ec2/main.tf
@@ -11,4 +11,15 @@ resource "aws_instance" "this" {
   }
 }
 
+resource "aws_instance" "test" {
+  ami           = "ami-0f95ad36d6d54ceba"
+  instance_type = "t2.micro"
+  #disable_api_termination = true
+  subnet_id              = var.public_subnet_ids[0]
+  vpc_security_group_ids = [var.ec2_sg]
+  key_name               = "aws-study-tf-key" #今回は既存のキーペアを使用
 
+  tags = {
+    Name = "${var.prefix}-ec2-CD-test"
+  }
+}
