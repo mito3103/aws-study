8c40d9f473f118f2e9b6a1ef07293b64bc5e6f72
mito <orixbaffaloes@gmail.com>
2025-09-18 15:55:44 +0900

    DBロックを設定

--- aws-study-tf/backend.tf
+++ aws-study-tf/backend.tf
@@ -1,7 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "aws-study-tf-605066737242"
-    region = "ap-northeast-1"
-    key    = "aws-study-tf/terraform.tfstate"
+    bucket         = "aws-study-tf-605066737242"
+    region         = "ap-northeast-1"
+    key            = "aws-study-tf/terraform.tfstate"
+    dynamodb_table = "aws-study-lock-table"
+    encrypt        = true
   }
 }
